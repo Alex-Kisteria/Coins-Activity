234f69f2e1b3697df1beddc7187dbe83f9d89856
Alexsus <alessandra.kisteria@gmail.com>
2024-12-08 15:27:03 +0800

    done

--- Coins-Activity/Form1.Designer.cs
+++ Coins-Activity/Form1.Designer.cs
@@ -30,36 +30,131 @@
         {
             menuStrip1 = new MenuStrip();
             coinsToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            countCoinsToolStripMenuItem = new ToolStripMenuItem();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            openFileDialog1 = new OpenFileDialog();
             menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // menuStrip1
             // 
             menuStrip1.ImageScalingSize = new Size(24, 24);
-            menuStrip1.Items.AddRange(new ToolStripItem[] { coinsToolStripMenuItem });
+            menuStrip1.Items.AddRange(new ToolStripItem[] { coinsToolStripMenuItem, countCoinsToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(800, 33);
+            menuStrip1.Size = new Size(1830, 33);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
+            menuStrip1.ItemClicked += menuStrip1_ItemClicked;
             // 
             // coinsToolStripMenuItem
             // 
+            coinsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem });
             coinsToolStripMenuItem.Name = "coinsToolStripMenuItem";
-            coinsToolStripMenuItem.Size = new Size(72, 29);
-            coinsToolStripMenuItem.Text = "Coins";
+            coinsToolStripMenuItem.Size = new Size(54, 29);
+            coinsToolStripMenuItem.Text = "File";
+            coinsToolStripMenuItem.Click += coinsToolStripMenuItem_Click;
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(158, 34);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // countCoinsToolStripMenuItem
+            // 
+            countCoinsToolStripMenuItem.Name = "countCoinsToolStripMenuItem";
+            countCoinsToolStripMenuItem.Size = new Size(125, 29);
+            countCoinsToolStripMenuItem.Text = "Count Coins";
+            countCoinsToolStripMenuItem.Click += countCoinsToolStripMenuItem_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Coint count:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(126, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(22, 25);
+            label2.TabIndex = 5;
+            label2.Text = "0";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 104);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 25);
+            label3.TabIndex = 6;
+            label3.Text = "Total Value:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(118, 104);
+            label4.Name = "label4";
+            label4.Size = new Size(22, 25);
+            label4.TabIndex = 7;
+            label4.Text = "0";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(61, 185);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(800, 800);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(952, 186);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(800, 800);
+            pictureBox2.TabIndex = 9;
+            pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.FileOk += openFileDialog1_FileOk_1;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1830, 1050);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
             Name = "Form1";
             Text = "Form1";
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -68,5 +163,14 @@
 
         private MenuStrip menuStrip1;
         private ToolStripMenuItem coinsToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem countCoinsToolStripMenuItem;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private OpenFileDialog openFileDialog1;
     }
 }
--- Coins-Activity/Form1.cs
+++ Coins-Activity/Form1.cs
@@ -1,10 +1,64 @@
+using System.Drawing;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+using ImageProcess2;
+
 namespace Coins_Activity
 {
     public partial class Form1 : Form
     {
+        Bitmap? loaded;
+        Bitmap? processed;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void coinsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            pictureBox1.Image = loaded = new Bitmap(openFileDialog1.FileName);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk_1(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            pictureBox1.Image = loaded = new Bitmap(openFileDialog1.FileName);
+        }
+
+        private void countCoinsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            BitmapFilter.Binary(loaded, processed, 200);
+            Coins.CountCoin(processed, ref label2, ref label4);
+
+            pictureBox2.Image = processed;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
